1604ee852989ec445957652407540b118880bcf4
Carlos Vazquez <carlosvazquez@users.noreply.github.com>
2017-12-04 08:14:17 -0600

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,22 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+const jobs = {
+  name: '',
+  role: '',
+  contacts: {
+    mobile: '',
+    email: '',
+    github: '',
+    twitter: '',
+    location: ''
+  },
+  welcomeMessage: '',
+  skills: ['aaa','aaa','aaaa'],
+  biopic: '',
+  display: function() {
+  	return '';
+	}
+
+};
